50ec2c937b405f05cbf4cf2b87d3c97681c1ba61
Tom Brisland <tombrisland@gmail.com>
2017-02-26 16:17:46 +0000

    added menu items and playlist examples

--- src/components/MenuDrawer/MenuDrawer.js
+++ src/components/MenuDrawer/MenuDrawer.js
@@ -1,5 +1,22 @@
 import React from 'react';
-import { AppBar, Drawer } from 'material-ui';
+import { AppBar, Drawer, List, ListItem, Subheader } from 'material-ui';
+import { ActionHome, AvAlbum, SocialPerson, ToggleStar, AvLibraryMusic } from 'material-ui/svg-icons';
+
+const primaryMenu = [
+  <ListItem key={ 0 } primaryText="Songs" leftIcon={ <AvLibraryMusic /> } />,
+  <ListItem key={ 1 }primaryText="Albums" leftIcon={ <AvAlbum/> } />,
+  <ListItem key={ 2 }primaryText="Artists" leftIcon={ <SocialPerson /> }/>,
+  <ListItem key={ 3 }primaryText="Starred" leftIcon={ <ToggleStar /> } />,
+];
+
+const playlists = [
+  <ListItem primaryText="Supa songz" secondaryText="Kapil chodehang"/>,
+  <ListItem primaryText="Grim" secondaryText="Kapil chodehang"/>,
+  <ListItem primaryText="Wow lol" secondaryText="Kapil chodehang"/>,
+  <ListItem primaryText="fels bod mun" secondaryText="Kapil chodehang"/>,
+  <ListItem primaryText="nope nope nope" secondaryText="Kapil chodehang"/>,
+  <ListItem primaryText="perhaps not" secondaryText="Kapil chodehang"/>,
+]
 
 class MenuDrawer extends React.Component {
 
@@ -20,8 +37,17 @@ class MenuDrawer extends React.Component {
         docked={ false }
         onRequestChange={ this.props.onRequestChange }
         children={
-          <AppBar
-            onLeftIconButtonTouchTap={ this.props.onRequestChange }/>
+          <div>
+            <AppBar
+              onLeftIconButtonTouchTap={ this.props.onRequestChange } />
+            <List>
+              <ListItem primaryText="Home" leftIcon={ <ActionHome /> } />
+              <Subheader>Music</Subheader>
+              { primaryMenu }
+              <Subheader>Playlists</Subheader>
+              { playlists }
+            </List>
+          </div>
         }
         />
     );
